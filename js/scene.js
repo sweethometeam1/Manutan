1fa8ff2f084f5926db949a8269d146c56e91ba56
Dmitry <Hr>
2019-07-05 15:41:20 +0300

    initial commit

---
+++ js/scene.js
@@ -0,0 +1,307 @@
+const solution = {
+  'id': '520',
+  'outline': [],
+  'data': {
+    'furniture_cad': [],
+    'furniture_info': [
+      {
+        'style': 'urban_9',
+        'name': 'sofa',
+        'image': '/assets/images/stage_4/urban_9/alternatives/sofa3sit_1_1.jpg',
+        'id': 'urban_9/sofa_01',
+        'url': 'https://dkbsj3nir6mzw.cloudfront.net/models/sofas/sofa_01|Assets/Prefabs/Sofas/Sofa_01.prefab',
+        'assetUrl': 'https://dkbsj3nir6mzw.cloudfront.net/models/sofas/sofa_01',
+        'assetName': 'Assets/Prefabs/Sofas/Sofa_01.prefab',
+        'info': [
+          'Houzz',
+          'Angled Leather Sofa'
+        ],
+        'price': 5800,
+        type: 'Sofas'
+      },
+      {
+        'style': 'urban_9',
+        'name': 'table',
+        'image': '/assets/images/stage_4/urban_9/alternatives/coffeetable_1_1.jpg',
+        'id': 'urban_9/table_01',
+        'url': 'https://dkbsj3nir6mzw.cloudfront.net/models/tables/table_01|Assets/Prefabs/Tables/Table_01.prefab',
+        'assetUrl': 'https://dkbsj3nir6mzw.cloudfront.net/models/tables/table_01',
+        'assetName': 'Assets/Prefabs/Tables/Table_01.prefab',
+        'info': [
+          'Houzz',
+          'Maki Chairs'
+        ],
+        'price': 520,
+        type: 'Tables'
+      },
+      {
+        'style': 'urban_9',
+        'name': 'carpet',
+        'image': '/assets/images/stage_4/urban_9/alternatives/coffeetable_1_1.jpg',
+        'id': 'urban_9/carpet_01',
+        'url': 'https://dkbsj3nir6mzw.cloudfront.net/models/carpets/carpet_01|Assets/Prefabs/Carpets/Carpet_01.prefab',
+        'assetUrl': 'https://dkbsj3nir6mzw.cloudfront.net/models/carpets/carpet_01',
+        'assetName': 'Assets/Prefabs/Carpets/Carpet_01.prefab',
+        'info': [
+          'Houzz',
+          'Maki Chairs'
+        ],
+        'price': 520,
+        type: 'Carpets'
+      },
+      {
+        'style': 'urban_9',
+        'name': 'tv_unit',
+        'image': '/assets/images/stage_4/urban_9/alternatives/coffeetable_1_1.jpg',
+        'id': 'urban_9/tv_unit_01',
+        'url': 'https://dkbsj3nir6mzw.cloudfront.net/models/tv_units/tv_unit_01|Assets/Prefabs/TV_Units/TV_Unit_01.prefab',
+        'assetUrl': 'https://dkbsj3nir6mzw.cloudfront.net/models/tv_units/tv_unit_01',
+        'assetName': 'Assets/Prefabs/TV_Units/TV_Unit_01.prefab',
+        'info': [
+          'Houzz',
+          'Maki Chairs'
+        ],
+        'price': 520,
+        type: 'TV_sets'
+      },
+    ],
+    'alternatives': {
+      'sofa': [
+        {
+          'style': 'urban_9',
+          'name': 'sofa',
+          'image': '/assets/images/stage_4/urban_9/alternatives/sofa3sit_1_1.jpg',
+          'id': 'urban_9/sofa_01',
+          'url': 'https://dkbsj3nir6mzw.cloudfront.net/models/sofas/sofa_01|Assets/Prefabs/Sofas/Sofa_01.prefab',
+          'assetUrl': 'https://dkbsj3nir6mzw.cloudfront.net/models/sofas/sofa_01',
+          'assetName': 'Assets/Prefabs/Sofas/Sofa_01.prefab',
+          'info': [
+            'Amazon',
+            'Leather Bench'
+          ],
+          'price': 1099,
+          type: 'Sofas'
+        },
+        {
+          'style': 'urban_9',
+          'name': 'sofa',
+          'image': '/assets/images/stage_4/urban_9/alternatives/sofa3sit_1_2.jpg',
+          'id': 'urban_9/sofa_02',
+          'url': 'https://dkbsj3nir6mzw.cloudfront.net/models/sofas/sofa_02|Assets/Prefabs/Sofas/Sofa_02.prefab',
+          'assetUrl': 'https://dkbsj3nir6mzw.cloudfront.net/models/sofas/sofa_02',
+          'assetName': 'Assets/Prefabs/Sofas/Sofa_02.prefab',
+          'info': [
+            'Amazon',
+            'Harrington Sofa in Grey'
+          ],
+          'price': 308.17,
+          type: 'Sofas'
+        },
+        {
+          'style': 'urban_9',
+          'name': 'sofa',
+          'image': '/assets/images/stage_4/urban_9/alternatives/sofa3sit_1_3.jpg',
+          'id': 'urban_9/sofa_03',
+          'url': 'https://dkbsj3nir6mzw.cloudfront.net/models/sofas/sofa_03|Assets/Prefabs/Sofas/Sofa_03.prefab',
+          'assetUrl': 'https://dkbsj3nir6mzw.cloudfront.net/models/sofas/sofa_03',
+          'assetName': 'Assets/Prefabs/Sofas/Sofa_03.prefab',
+          'info': [
+            'Mod shop',
+            'Koenig Sofa, Pewter Velvet'
+          ],
+          'price': 3145,
+          type: 'Sofas'
+        },
+        {
+          'style': 'urban_9',
+          'name': 'sofa',
+          'image': '/assets/images/stage_4/urban_9/alternatives/sofa3sit_1_4.jpg',
+          'id': 'urban_9/sofa_04',
+          'url': 'https://dkbsj3nir6mzw.cloudfront.net/models/sofas/sofa_04|Assets/Prefabs/Sofas/Sofa_04.prefab',
+          'assetUrl': 'https://dkbsj3nir6mzw.cloudfront.net/models/sofas/sofa_04',
+          'assetName': 'Assets/Prefabs/Sofas/Sofa_04.prefab',
+          'info': [
+            'Kohl\'s',
+            'Mid-Century Modern Sofa'
+          ],
+          'price': 479.99,
+          type: 'Sofas'
+        }
+      ],
+      'table': [
+        {
+          'style': 'urban_9',
+          'name': 'table',
+          'image': '/assets/images/stage_4/urban_9/alternatives/coffeetable_1_1.jpg',
+          'id': 'urban_9/table_01',
+          'url': 'https://dkbsj3nir6mzw.cloudfront.net/models/tables/table_01|Assets/Prefabs/Tables/Table_01.prefab',
+          'assetUrl': 'https://dkbsj3nir6mzw.cloudfront.net/models/tables/table_01',
+          'assetName': 'Assets/Prefabs/Tables/Table_01.prefab',
+          'info': [
+            'Amazon',
+            'Antiqued Coffee Table'
+          ],
+          'price': 499,
+          type: 'Tables'
+        },
+        {
+          'style': 'urban_9',
+          'name': 'table',
+          'image': '/assets/images/stage_4/urban_9/alternatives/coffeetable_1_2.jpg',
+          'id': 'urban_9/table_02',
+          'url': 'https://dkbsj3nir6mzw.cloudfront.net/models/tables/table_02|Assets/Prefabs/Tables/Table_02.prefab',
+          'assetUrl': 'https://dkbsj3nir6mzw.cloudfront.net/models/tables/table_02',
+          'assetName': 'Assets/Prefabs/Tables/Table_02.prefab',
+          'info': [
+            'Amazon',
+            'Vintage Coffee Table'
+          ],
+          'price': 86.99,
+          type: 'Tables'
+        }
+      ],
+      'carpet': [
+        {
+          'style': 'urban_9',
+          'name': 'carpet',
+          'image': '/assets/images/stage_4/urban_9/alternatives/coffeetable_1_1.jpg',
+          'id': 'urban_9/carpet_01',
+          'url': 'https://dkbsj3nir6mzw.cloudfront.net/models/carpets/carpet_01|Assets/Prefabs/Carpets/Carpet_01.prefab',
+          'assetUrl': 'https://dkbsj3nir6mzw.cloudfront.net/models/carpets/carpet_01',
+          'assetName': 'Assets/Prefabs/Carpets/Carpet_01.prefab',
+          'info': [
+            'Houzz',
+            'Maki Chairs'
+          ],
+          'price': 520,
+          type: 'Carpets'
+        },
+        {
+          'style': 'urban_9',
+          'name': 'carpet',
+          'image': '/assets/images/stage_4/urban_9/alternatives/coffeetable_1_1.jpg',
+          'id': 'urban_9/carpet_02',
+          'url': 'https://dkbsj3nir6mzw.cloudfront.net/models/carpets/carpet_02|Assets/Prefabs/Carpets/Carpet_02.prefab',
+          'assetUrl': 'https://dkbsj3nir6mzw.cloudfront.net/models/carpets/carpet_02',
+          'assetName': 'Assets/Prefabs/Carpets/Carpet_02.prefab',
+          'info': [
+            'Houzz',
+            'Maki Chairs'
+          ],
+          'price': 520,
+          type: 'Carpets'
+        },
+        {
+          'style': 'urban_9',
+          'name': 'carpet',
+          'image': '/assets/images/stage_4/urban_9/alternatives/coffeetable_1_1.jpg',
+          'id': 'urban_9/carpet_03',
+          'url': 'https://dkbsj3nir6mzw.cloudfront.net/models/carpets/carpet_03|Assets/Prefabs/Carpets/Carpet_03.prefab',
+          'assetUrl': 'https://dkbsj3nir6mzw.cloudfront.net/models/carpets/carpet_03',
+          'assetName': 'Assets/Prefabs/Carpets/Carpet_03.prefab',
+          'info': [
+            'Houzz',
+            'Maki Chairs'
+          ],
+          'price': 520,
+          type: 'Carpets'
+        },
+      ],
+      'tv_unit': [
+        {
+          'style': 'urban_9',
+          'name': 'tv_unit',
+          'image': '/assets/images/stage_4/urban_9/alternatives/coffeetable_1_1.jpg',
+          'id': 'urban_9/tv_unit_01',
+          'url': 'https://dkbsj3nir6mzw.cloudfront.net/models/tv_units/tv_unit_01|Assets/Prefabs/TV_Units/TV_Unit_01.prefab',
+          'assetUrl': 'https://dkbsj3nir6mzw.cloudfront.net/models/tv_units/tv_unit_01',
+          'assetName': 'Assets/Prefabs/TV_Units/TV_Unit_01.prefab',
+          'info': [
+            'Houzz',
+            'Maki Chairs'
+          ],
+          'price': 520,
+          type: 'TV_sets'
+        },
+        {
+          'style': 'urban_9',
+          'name': 'tv_unit',
+          'image': '/assets/images/stage_4/urban_9/alternatives/coffeetable_1_1.jpg',
+          'id': 'urban_9/tv_unit_02',
+          'url': 'https://dkbsj3nir6mzw.cloudfront.net/models/tv_units/tv_unit_02|Assets/Prefabs/TV_Units/TV_Unit_02.prefab',
+          'assetUrl': 'https://dkbsj3nir6mzw.cloudfront.net/models/tv_units/tv_unit_02',
+          'assetName': 'Assets/Prefabs/TV_Units/TV_Unit_02.prefab',
+          'info': [
+            'Houzz',
+            'Maki Chairs'
+          ],
+          'price': 520,
+          type: 'TV_sets'
+        }
+      ]
+    }
+  },
+  'ctime': new Date('2019-06-11T13:47:21.361701Z'),
+  'style': 'urban_9',
+  'image': '/assets/images/stage_4/urban_9/reference.jpg',
+  'owner': 'f97cd08d-19e7-4f01-afbf-bc3902bd81aa'
+};
+let unityInstance = null;
+let loadedItemsCount = 0;
+let infoPopupData = null;
+
+window.loadUnitySetup = () => {
+  console.log('scene loading time', Date.now() - this.startDate);
+  solution.data.furniture_info.map(f =>
+    unityInstance.SendMessage(
+      f.type,
+      'Load',
+      f.assetUrl + '|' + f.assetName + '|' + f.id + '|true'
+    )
+  );
+};
+window.unityItemLoaded = () => {
+  loading = false;
+  loadedItemsCount++;
+  if (loadedItemsCount === solution.data.furniture_info.length) {
+    // stopWaiting();
+  }
+};
+window.openPopup = (x, y, id) => {
+  infoPopupData = {
+    x,
+    y,
+    item: solution.data.furniture_info.find(f => f.id === id)
+  };
+}
+window.addEventListener('keydown', (event) => {
+  infoPopupData = null;
+});
+window.addEventListener('mousedown', (event) => {
+  infoPopupData = null;
+});
+window.addEventListener('mousewheel', (event) => {
+  infoPopupData = null;
+});
+
+$(document).on('click', '.visual-buttons__item .ddd', () => {
+  unityInstance.SendMessage('CameraSwitch', 'ChangeCamera', 'front')
+})
+
+$(document).on('click', '.visual-buttons__item .steps', () => {
+  unityInstance.SendMessage('CameraSwitch', 'ChangeCamera', 'orbit')
+})
+
+/*$(document).on('click', '.card-item__square', () => {
+  unityInstance = UnityLoader.instantiate(
+    'unityContainer',
+    '/scenes/Build/Build.json',
+  );
+})*/
+
+$(document).ready(() => {
+  unityInstance = UnityLoader.instantiate(
+    'unityContainer',
+    '/scenes/Build/Build.json',
+  );
+})
